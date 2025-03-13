28c1fe6013cca7fb1ed8ad3147fcdd473c100979
Arthur <crfernandesarthur@gmail.com>
2025-03-13 15:47:09 -0300

    initial commit

---
+++ vuetify/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app theme="light">
+    <VAppBar flat class="border-b" title="My app"></VAppBar>
+    <v-main>
+      <HelloWorld />
+    </v-main>
+
+    <AppFooter />
+  </v-app>
+</template>
+
+<script setup>
+  //
+</script>
